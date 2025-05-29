75f1c00d7b8bd0feaec0cb92c598b70084c4e21a
nightbop9 <teixeiravitorinoc8@gmail.com>
2025-05-28 23:00:34 -0300

    fix:logica de upload e atualizacao envolvendo imagens e associacoes resolvidas

--- src/main/java/com/api/skinpro/config/WebConfig.java
+++ src/main/java/com/api/skinpro/config/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://apimemoriaviva.azurewebsites.net",
-                        "http://127.0.0.1:5500"
-                )
+                .allowedOriginPatterns("*")  // Permite qualquer origem
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
